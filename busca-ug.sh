7b62a72740269f80d2aade5b706df53db84e828c
Pedro Romero Rodríguez <notewhitesongblack@gmail.com>
2012-11-05 18:54:18 -0600

    Busca-ug.sh
    Se renombro el archivo por que se le añade la funcion de buscar
    usuarios y grupo ahora se resiver dos parametros si solo resive
    uno se busca por defecto en usuarios, con el segundo paramentros
    busca grupos tambien

--- busca-ug.sh
+++ busca-ug.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 var=${1:?"Se necesita grupo"}
+busqueda=${2:-"usuario"}
 
-var=$(egrep "^$var" /etc/group)
+if [[ $busqueda == "usuario" ]]; then
+	archivo="/etc/passwd"
+else
+	archivo="/etc/group"
+fi
+
+var=$(egrep "^$var" ${archivo})
 
 if [[ $var ]]; then
 	echo -e "Existe el Grupo $1 \n :P "
