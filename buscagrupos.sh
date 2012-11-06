5616f3aaf3659d869c4eadaa4dd62875b967a090
Pedro Romero Rodríguez <notewhitesongblack@gmail.com>
2012-11-05 18:34:28 -0600

    Buscar Grupos
    Este scrip de bash permite buscar grupos en tu maquina o cumpudora
    dando como para metro un cadena a la cual se hare al busqueda

--- buscagrupos.sh
+++ buscagrupos.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-var=$(egrep "^$1" /etc/group)
+
+var=${1:?"Se necesita grupo"}
+
+var=$(egrep "^$var" /etc/group)
 
 if [[ $var ]]; then
-	echo "Existe el Grupo :P "$1
+	echo -e "Existe el Grupo $1 \n :P "
 else
-	echo "No existe el grupo :S"$1
+	echo -e "No existe el grupo $1 \n :S "
 fi
-
-#cat /etc/group | egrep '\busers'
-#cat /etc/group | egrep '^users'
-#variable=$(egrep '^user' /etc/group)
-
-#grupo=$(cat /etc/group | egrep 'b'$1':' )
-
-#if [[ $grupo ]]; then
-#	echo "Existe Grupo: "$1
-#else
-#	echo "No existe grupo: "$1
-#if
-
